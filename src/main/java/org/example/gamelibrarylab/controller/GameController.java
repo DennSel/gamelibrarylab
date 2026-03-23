d6a9ba2c9e2bbe0491addae84c54900f10230843
Dennis Selden <111012436+DennSel@users.noreply.github.com>
2026-03-23 22:51:16 +0100

    Implement display all games list

--- src/main/java/org/example/gamelibrarylab/controller/GameController.java
+++ src/main/java/org/example/gamelibrarylab/controller/GameController.java
@@ -1,8 +1,13 @@
 package org.example.gamelibrarylab.controller;
 
+import org.example.gamelibrarylab.dto.GameDTO;
 import org.example.gamelibrarylab.service.GameService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/games")
@@ -11,6 +16,13 @@ public class GameController {
 
     public GameController(GameService service) {
         this.service = service;
+    }
+
+    @GetMapping
+    public String list(Model model) {
+        List<GameDTO> games = service.getAllGames();
+        model.addAttribute("games", games);
+        return "games/list";
     }
 }
 
