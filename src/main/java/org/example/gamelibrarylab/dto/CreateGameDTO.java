1f032ca718eecd331e0474eb07f45c3c8f9a3e36
Dennis Selden <111012436+DennSel@users.noreply.github.com>
2026-03-23 14:50:18 +0100

    Implement CreateGameDTO

---
+++ src/main/java/org/example/gamelibrarylab/dto/CreateGameDTO.java
@@ -0,0 +1,14 @@
+package org.example.gamelibrarylab.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+
+import java.time.LocalDate;
+
+public record CreateGameDTO(
+        @NotBlank String title,
+        @NotBlank String publisher,
+        @NotNull @PastOrPresent LocalDate releaseDate,
+        @NotBlank String developer
+        ) {}
