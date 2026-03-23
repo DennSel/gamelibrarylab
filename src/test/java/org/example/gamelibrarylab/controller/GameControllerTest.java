90da5fa4ab0f7fd10ce9180ccaa41104ee034086
Dennis Selden <111012436+DennSel@users.noreply.github.com>
2026-03-23 23:31:36 +0100

    Add GameControllerTest for testing game list view

---
+++ src/test/java/org/example/gamelibrarylab/controller/GameControllerTest.java
@@ -0,0 +1,38 @@
+package org.example.gamelibrarylab.controller;
+
+import org.example.gamelibrarylab.service.GameService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+
+@WebMvcTest(GameController.class)
+public class GameControllerTest {
+    @Autowired
+    MockMvc mvc;
+
+    @MockitoBean
+    GameService service;
+
+    @Test
+    void listReturnsCorrectView() throws Exception {
+        when(service.getAllGames()).thenReturn(List.of());
+
+        mvc.perform(get("/games"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("games/list"))
+                .andExpect(model().attributeExists("games"));
+    }
+}
+
