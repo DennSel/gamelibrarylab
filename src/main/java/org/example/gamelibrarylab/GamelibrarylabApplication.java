f4ffb03e9a09d681d88a9db1de1887056660d20c
Dennis Selden <111012436+DennSel@users.noreply.github.com>
2026-03-23 13:43:18 +0100

    Initial setup

---
+++ src/main/java/org/example/gamelibrarylab/GamelibrarylabApplication.java
@@ -0,0 +1,13 @@
+package org.example.gamelibrarylab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelibrarylabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamelibrarylabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/gamelibrarylab/GamelibrarylabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gamelibrarylab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelibrarylabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
