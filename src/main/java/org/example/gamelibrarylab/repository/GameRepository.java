848b54bb9d3b1ee45e67682426a2789d6680abae
Dennis Selden <111012436+DennSel@users.noreply.github.com>
2026-03-24 01:12:16 +0100

    Add search functionality to GameRepository

--- src/main/java/org/example/gamelibrarylab/repository/GameRepository.java
+++ src/main/java/org/example/gamelibrarylab/repository/GameRepository.java
@@ -4,6 +4,8 @@ import jakarta.validation.constraints.NotBlank;
 import org.example.gamelibrarylab.entity.Game;
 import org.springframework.data.repository.ListCrudRepository;
 
+import java.util.List;
+
 public interface GameRepository extends ListCrudRepository<Game, Long> {
     boolean existsByTitleAndDeveloper(
             @NotBlank
@@ -11,5 +13,11 @@ public interface GameRepository extends ListCrudRepository<Game, Long> {
             @NotBlank
             String developer
     );
+
+    List<Game> findByTitleContainingIgnoreCaseOrDeveloperContainingIgnoreCase(
+            String title, String developer
+    );
 }
+
+
 
